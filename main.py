949966a1006d1b4f7d66b71d6372e0c3e389f45a
Sergio Majé <smajefranco@gmail.com>
2025-08-14 14:57:38 -0500

    ✨ Add records exporting to CSV and JSON files

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 from pathlib import Path
-from typing import Final
+from typing import Final, cast
 
 from app.factories import ServiceFactory
+from app.interfaces import ExportDataInterface
+from app.services.service import ServiceABC
 
 
 MIN_CODE_PROCEDURE_LENGTH: Final = 4
 MAX_CODE_PROCEDURE_LENGTH: Final = 5
 MIN_CODE_GROUP_LENGTH: Final = 1
 MAX_CODE_GROUP_LENGTH: Final = 2
+SOAT_NOTE_AND_RESTRICTIONS: Final = """
+NOTA: El costo total es una tarifa sugerida.
+Esta depende de que halla hecho el procedimiento.
+El cual debe estar justificado y firmando por el profesional que lo realizó.
+Los valores en cero (0) tienen restricciones para ese grupo quirúrgico.
+"""
 
 
-def main():
+def main():  # noqa: PLR0912
   """Main function to run the SOAT 2025 data extraction application."""
   print("Aplicación Tarifario SOAT 2025")
 
   print("Cargando tarifario SOAT 2025...")
   factory = ServiceFactory()
+  exporters: list[ExportDataInterface] = [
+    factory.anesthesiologist,
+    factory.surgeon,
+    factory.procedure,
+    factory.assistant,
+    factory.material,
+    factory.operating_room,
+    factory.pre_consultation,
+    factory.cost_aggregator,
+  ]
   print("Tarifario cargado correctamente.")
 
   while True:
@@ -24,6 +42,8 @@ def main():
       "1. Buscar procedimiento por código\n"
       "2. Buscar grupo quirúrgico\n"
       "3. Ver todos los grupos quirúrgicos\n"
+      "4. Exportar registro a un archivo CSV\n"
+      "5. Exportar registro a un archivo JSON\n"
       "0. Salir\n"
       "Opción: "
     )
@@ -50,11 +70,7 @@ def main():
 
       print(f"Grupo quirúrgico encontrado:\n{group}\n")
       print(f"Costo total del grupo quirúrgico: $ {group['total'].iloc[0]:,.0f} COP\n")
-      print(
-        "NOTA: El costo total es una tarifa sugerida.\n",
-        "Esta depende de que halla hecho el procedimiento.",
-        "El cual debe estar justificado y firmando por el profesional que lo realizó.\n"
-      )
+      print(SOAT_NOTE_AND_RESTRICTIONS)
     elif choice == "2":
       group_code = input("Ingrese el código del grupo quirúrgico: ").strip()
       if not (MIN_CODE_GROUP_LENGTH <= len(group_code) <= MAX_CODE_GROUP_LENGTH):
@@ -70,19 +86,22 @@ def main():
 
       print(f"Grupo quirúrgico encontrado:\n{group}\n")
       print(f"Costo total del grupo quirúrgico: $ {group['total'].iloc[0]:,.0f} COP\n")
-      print(
-        "NOTA: El costo total es una tarifa sugerida.\n",
-        "Esta depende de que halla hecho el procedimiento.",
-        "El cual debe estar justificado y firmando por el profesional que lo realizó.\n"
-      )
+      print(SOAT_NOTE_AND_RESTRICTIONS)
     elif choice == "3":
       print(f"Grupos quirúrgicos disponibles:\n{factory.cost_aggregator.data}\n")
-      print(
-        "NOTA: El costo total es una tarifa sugerida.\n",
-        "Esta depende de que halla hecho el procedimiento.",
-        "El cual debe estar justificado y firmando por el profesional que lo realizó.\n",
-        "Los valores en cero (0) tienen restricciones para ese grupo quirúrgico.\n",
-      )
+      print(SOAT_NOTE_AND_RESTRICTIONS)
+    elif choice == "4":
+      print("Exportando registros a CSV...")
+      for exporter in exporters:
+        filename = cast(ServiceABC, exporter).column
+        exporter.export_to_csv(Path(f"output/{filename}.csv"))
+      print("Registros exportados a CSV correctamente.")
+    elif choice == "5":
+      print("Exportando registros a JSON...")
+      for exporter in exporters:
+        filename = cast(ServiceABC, exporter).column
+        exporter.export_to_json(Path(f"output/{filename}.json"))
+      print("Registros exportados a JSON correctamente.")
     elif choice == "0":
       break
     else:
